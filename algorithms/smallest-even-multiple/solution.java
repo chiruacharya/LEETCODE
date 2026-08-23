6171ae677e3c0ec9a3f51b33fcded0fd047975af
Chiranjeevi achari <chiranjeeviacharya966@gmail.com>
2026-08-23 23:40:58 +0530

    feat: add Smallest Even Multiple solution

--- algorithms/smallest-even-multiple/solution.java
+++ algorithms/smallest-even-multiple/solution.java
@@ -1,22 +1,11 @@
 class Solution {
-    public boolean checkDivisibility(int n) {
+    public int smallestEvenMultiple(int n) {
         // COMPLETELY OWN SOLVED
-        int sum = 0;
-        int product = 1;
-        int temp = n;
-        while(n>0){
-            int digit = n%10;
-            sum+=digit;
-            product *= digit;
-            n= n/10;
-        }
-        int total = sum + product;
-        if (temp%total ==0){
-            return true;
+        if(n%2==0){
+            return n;
         }
         else{
-            return false;
+            return n*2;
         }
-
     }
 }
